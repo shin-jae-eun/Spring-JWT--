40193d749141d4c706985e2880df60955fc03477
shin-jae-eun <sshinjaeeunn@gmail.com>
2024-06-29 21:46:51 +0900

    JWT Token 구현 완료

--- SpringJWT/config/jwt/JwtProperties.java
+++ SpringJWT/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.jwt.SpringJWT.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "cos"; //우리 서버만 알고 있는 비밀 값
-    int EXPIRATION_TIME = 60000 * 10; // 만료 시간
+    int EXPIRATION_TIME = 60000 * 10;  // 만료 시간
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
